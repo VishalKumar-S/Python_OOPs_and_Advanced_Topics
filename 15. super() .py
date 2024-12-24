903856d867fdaa01799e6a3c7d4909d3ef815d7a
Vishal Kumar. S <vk28.02.2004@gmail.com>
2024-12-24 17:00:32 +0530

    Completed entire super() notes

--- 15. super() .py
+++ 15. super() .py
@@ -1,17 +1,42 @@
 class human():
     ''' super() method is to invoke the parent class method, by the child class method, for the purpose of child class. Maina dvantage of using it is COde reusability. Instead of writing the same common code,again and agian in the child class, we can initlaise it in the parent class,adn use super to use it. When the child inherits multiple parents, and all those parents have the same method name, then super will order the MRO algorithm to choose the correct method class. But if u  need to call a specific parent's method, instead fo floowing the MRO order of the method lookup,htere are 2 ways to call specific parent;s method name.
     1. parent_Class_name.method_name(self) 
-    2. super__(Class_name, self).method_name() Here it won't call the method of class_name, scine we used super(), it goes the the parent of the class_name adn uses that's method'''
+    2. super__(Class_name, self).method_name() Here it won't call the method of class_name, scine we used super(), it goes the the parent of the class_name adn uses that's method.
+    
+    When you use super(), you're working with the parent class's methods and class variables, but NOT directly with instance variables. Instance variables are always bound to self.
+    '''
+
+    no_of_hands = 2
+    no_of_legs = 2
+
+    def face(self):
+        self.face_shape = 'Oval'
 
     def __init__(self,name,age):
         print("You are using human construcotr")
         self.name = name
         self.age = age
+        self.eyes = 2
+
+        print(name,age)
     
     def display(self):
         print("Display function of human")
         print("Name: ",self.name)
         print("Age: ",self.age)
+
+    @staticmethod
+    def static_method():
+        print("Im parent static method")
+
+    @classmethod
+    def class_method(cls):
+        print("Im parent class method")
+
+    def instance_method(self):
+        print("Im parent instance method")
+
+
 
 class School_members(human):
     def __init__(self,school_name,id_no, bus_no):
@@ -34,10 +59,75 @@ class Teacher(human):
         self.subject = subject
         self.experience = experience
 
+
+
     def display(self):
         super().display()
         print("Subject: ",self.subject)
         print("Experience: ",self.experience)
+        print(f"I can use static/class variables of parent within child, with just using super().I have {super().no_of_hands} hands and {super().no_of_legs} legs")
+        print(f"I have {self.eyes} no of eyes. eyes is an instanve variable in the constructor of the human class. So,to access it we need to have self, can't do with super()")
+        try:
+            print(super().face_shape)
+        except Exception as e:
+            print("Error is ",e,"The reason is, u can't call a parent class instance variable directly, using super() in child class, u need to use self")
+            try:
+                print("My face shape is",self.face_shape)
+            
+            except Exception as e:
+                print("The error is ",e,"When you use super(), you're working with the parent class's methods and class variables, but NOT directly with instance variables. Instance variables are always bound to self. If the face_shape var., was within the constructor, the face_type var., woudl have been initlaited automatically, when we r using the our self, we would have used it directly, but here face() is an instance method. So, we would need to call the instance method to initialise the instance_varibale")
+                self.face()
+                print("My face shape is",self.face_shape)
+            
+    @staticmethod
+    def static_method():
+        print("Im child static method,trying to access parent's satic,class and instance methods")
+        try:
+            super().static_method()
+            super().class_method()
+        except Exception as e:
+            print("Error is",e,"The error RuntimeError: super(): no arguments occurs because the super() function requires information about the class and instance from which it is called, which it cannot infer in a static method. In a static method, there's no automatic way to get the current class or instance because static methods don't receive self or cls as parameters.")
+            human.static_method()
+            human.class_method()
+            print("OR")
+            super(Teacher,Teacher).static_method()
+            super(Teacher,Teacher).class_method()
+
+        try:
+            super().instance_method()
+        except Exception as e:
+            print("Error is,",e,"You cannot call a instance method without self. If u want to call the isntance method wihitn static method, u need to pass teh ref., var explicitely to this staic method to call the instance method")
+            human.instance_method(Teacher("Durga", 40, "Python", 10))
+
+
+
+    @classmethod
+    def class_method(cls):
+        print("Im child class method,trying to access parent's satic,class and instance methods")
+
+        print("When you call super() in a class method, you can use cls to provide the class context, and it will resolve the method based on the MRO.")
+        super().static_method()
+        super().class_method()
+
+
+        print("OR")
+        super(cls,cls).static_method()
+        super(cls,cls).class_method()
+
+        try:
+            super().instance_method()
+        except Exception as e:
+            print("Error is,",e,"You cannot call a instance method without self. If u want to call the isntance method wihitn class method, u need to pass teh ref., var explicitely to this staic method to call the instance method")  
+            print("here, it is how: ")
+            super(Teacher,cls("Durga Sir",40,"Python",10)).instance_method()
+            print("OR")
+            human.instance_method(cls("Durga Sir",40,"Python",10))
+
+    def instance_method(self):
+        print("Im child instance method,trying to access parent's satic,class and instance methods")    
+        super().instance_method()
+        super().static_method()
+        super().class_method()
       
 print(human.__doc__)
 
@@ -81,8 +171,12 @@ class Student(School_members):
 
 
 
+
 print(Teacher.__doc__)
 t = Teacher("Shreekanth Verma Chekuri",40,'ML',10)
 t.display()
+t.static_method()
+t.class_method()
+t.instance_method()
 s = Student("Vishal",20,'III')
 s.display()
